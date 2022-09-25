8f424ec7951225b59bc5bb5a7caede163087be65
yunguks <walk1009@naver.com>
2022-09-25 18:30:06 +0900

    update

--- ISR/EDSR_trian.py
+++ ISR/EDSR_trian.py
@@ -18,18 +18,16 @@ print(f'torch version : {torch.__version__}')
 
 import math
 def cal_psnr(img1,img2):
-    x = img1.detach().cpu().numpy()
-    y = img2.detach().cpu().numpy()
     PIXEL_MAX = 1.0
     # # input shaep가 [ num_patch, 3, 64, 64] 일경우
-    p = 0
-    for i in range(img1.shape[0]):
+    #p = 0
+    #for i in range(img1.shape[0]):
         #PSNR구하는 코드
-        p += 10 * math.log10(PIXEL_MAX / np.mean((x[i] - y[i]) ** 2) )
-    return p/img1.shape[0]
+    #    p += 10 * math.log10(PIXEL_MAX / np.mean((x[i] - y[i]) ** 2) )
+    #return p/img1.shape[0]
 
     # input shape가 [3, 64, 64] 일 경우
-    # return 10 * math.log10(PIXEL_MAX / np.mean((x - y) ** 2) )
+    return 10 * math.log10(PIXEL_MAX / np.mean((img1 - img2) ** 2) )
 
 def seed_everything(seed):
     random.seed(seed)
@@ -47,34 +45,76 @@ class CustomDataset(torch.utils.data.Dataset):
         self.low_list = img_list['LR']
         if self.train_mode:
             self.high_list = img_list['HR']
+        self.patch_size = 256//4
 
     def __getitem__(self,index,check=False):
-        n = random.randrange(64)
-        low_path = './data'+self.low_list.iloc[index][1:]
+        h = random.randrange(255-self.patch_size)
+        w = random.randrange(255-self.patch_size)
+        low_path = './Dataset'+self.low_list.iloc[index][1:]
         low_img = cv2.imread(low_path)
 
+        low_img = low_img[h:h+self.patch_size,w:w+self.patch_size, :]
+
+        if low_img is None:
+            print(f'{low_path} is exist? {os.path.isfile(low_path)}')
         if check:
             print(f'image path : {low_path}')
             print(f'image shape : {low_img.shape}')
-            print(F'image type : {type(low_img)}')
-        
+            print(f'image type : {type(low_img)}')
+
         if self.transform is not None:
             torch.manual_seed(41)
             low_img=self.transform(low_img)
 
         
         if self.train_mode:
-            high_path = './data'+self.high_list.iloc[index][1:]
+            high_path = './Dataset'+self.high_list.iloc[index][1:]
             high_img = cv2.imread(high_path)
+            high_img = high_img[4*h:4*h+self.patch_size*4, 4*w:4*w+self.patch_size*4, :]
             if self.transform is not None:
                 torch.manual_seed(41)
                 high_img = self.transform(high_img)
             return low_img, high_img
-        else:
-            file_name = low_path.split('/')[-1]
-                
-            return low_img, file_name
+        
+        file_name = low_path.split('/')[-1]    
+        return low_img, file_name
     
+    def __len__(self):
+        return len(self.low_list)
+
+class test_CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, img_list, transform=None, train_mode=False):
+        self.transform = transform
+        self.low_list = img_list['LR']
+        self.train_mode = train_mode
+        if self.train_mode:
+            self.high_list = img_list['HR']
+
+    def __getitem__(self,index,check=False):
+        low_path = './Dataset'+self.low_list.iloc[index][1:]
+        low_img = cv2.imread(low_path)
+        if check:
+            print(f'image path : {low_path}')
+            print(f'image shape : {low_img.shape}')
+            print(f'image type : {type(low_img)}')
+
+        if self.transform is not None:
+            torch.manual_seed(41)
+            low_img=self.transform(low_img)
+
+        if self.train_mode:
+            high_path = './Dataset'+self.high_list.iloc[index][1:]
+            high_img = cv2.imread(high_path)
+            if self.transform is not None:
+                torch.manual_seed(41)
+                high_img=self.transform(high_img)
+            
+            return low_img, high_img
+
+        file_name = low_path.split('/')[-1]
+                
+        return low_img, file_name
+
     def __len__(self):
         return len(self.low_list)
 
@@ -83,7 +123,6 @@ seed_everything(41)
 train_transform = torchvision.transforms.Compose([
     torchvision.transforms.ToPILImage(),
     torchvision.transforms.RandomHorizontalFlip(),
-    torchvision.transforms.RandomVerticalFlip(),
     torchvision.transforms.ToTensor(),
 ])
 
@@ -91,6 +130,9 @@ test_transform = torchvision.transforms.Compose([
     torchvision.transforms.ToPILImage(),
     torchvision.transforms.ToTensor(),
 ])
+
+from torch import nn
+from einops.layers.torch import Rearrange
 
 class MeanShift(nn.Module):
     def __init__(self, mean_rgb, sub):
@@ -118,19 +160,19 @@ class ResBlock(nn.Module):
         super(ResBlock, self).__init__()
 
         self.act = nn.ReLU(True)
-        self.conv1 = nn.Conv2d(n_feats, n_feats,kernel_size =3,bias=True , padding =1)
-        self.conv2 = nn.Conv2d(n_feats, n_feats,kernel_size =3,bias=True , padding =1)
+        self.conv1 = nn.Conv2d(n_feats, n_feats,kernel_size =3, padding =1)
+        self.conv2 = nn.Conv2d(n_feats, n_feats,kernel_size =3, padding =1)
         self.res_scale = res_scale
 
     def forward(self, x):
-        res = self.act(x)
-        res = self.conv1(res)
+        res = self.conv1(x)
+        res = self.act(res)
         res = self.conv2(res).mul(self.res_scale)
         res += x
         return res
 
 class EDSR(nn.Module):
-    def __init__(self, scale_factor=2*2, num_channels=3, num_feats=256, num_blocks=32, res_scale=0.1,img_mean=(0.4411, 0.479, 0.4414)):
+    def __init__(self, scale_factor=2*2, num_channels=3, num_feats=256, num_blocks=32, res_scale=0.1, img_mean=(0.4411, 0.479, 0.4414)):
         super(EDSR, self).__init__()
 
         self.sub_mean = MeanShift(img_mean, sub=True)
@@ -142,10 +184,10 @@ class EDSR(nn.Module):
         self.tail = nn.Sequential(
             nn.Conv2d(num_feats, num_feats * (2**2), kernel_size=3, stride=1, padding=1), 
             nn.PixelShuffle(2),
-            nn.ReLU(),
+            #nn.ReLU(True),
             nn.Conv2d(num_feats, num_feats * (2**2), kernel_size=3, stride=1, padding=1), 
             nn.PixelShuffle(2),
-            nn.ReLU(),
+            #nn.ReLU(True),
             nn.Conv2d(num_feats, num_channels, kernel_size=3, stride=1, padding=1),
         )
         self.initialize_weights()
@@ -159,6 +201,7 @@ class EDSR(nn.Module):
         x = self.add_mean(x)
 
         return x
+
     def initialize_weights(self):
         # track all layers
         for m in self.modules():
@@ -172,7 +215,7 @@ class EDSR(nn.Module):
 
 BATCH_SIZE =16
 
-train_list = pd.read_csv('./data/train_patch.csv')
+train_list = pd.read_csv('./Dataset/train.csv')
 train_list = train_list[0:int(len(train_list)*0.75)]
 val_list = train_list[int(len(train_list)*0.75):]
 print(f'train data : {len(train_list)}, val data : {len(val_list)}')
@@ -188,33 +231,31 @@ val_dataset = CustomDataset(val_list,test_transform,train_mode=True)
 val_loader = torch.utils.data.DataLoader(val_dataset,BATCH_SIZE,False)
 
 
-test_list = pd.read_csv('./data/test.csv')
+test_list = pd.read_csv('./Dataset/test.csv')
 print(f'test data : {len(test_list)}')
 test_dataset = CustomDataset(test_list,test_transform,train_mode=False)
 test_loader = DataLoader(test_dataset,BATCH_SIZE,False)
 
 model = EDSR()
 criterion = nn.L1Loss()
-optimizer = torch.optim.Adam(params=model.parameters(), lr =0.00005)
-
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0002)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5000, gamma=0.5)
 scheduler = None
 print('parameter set')
 
 from tqdm import tqdm
 
-history = {'loss':[],'train_psnr':[],'val_psnr':[]}
+history = {'loss':[],'val_psnr':[]}
 best_psnr = 0.0
 count =1
-EPOCH = 200
+EPOCH = 1000
 
 print('start train')
 torch.cuda.empty_cache()
 model.to(device)
 print(torch.cuda.memory_allocated() / 1024 /1024)
-model.train()
 for epoch in range(1, EPOCH+1):
     running_loss = 0
-    train_psnr = 0
 
     model.train()
     for lr, hr in tqdm(iter(train_loader)):
@@ -224,46 +265,58 @@ for epoch in range(1, EPOCH+1):
 
         optimizer.zero_grad()
         out = model(lr_img)
+
         loss = criterion(out,hr_img)
-        train_psnr += cal_psnr(out,hr_img)
 
         loss.backward()
-        optimizer.step()
         running_loss +=loss.item()
-
-    train_psnr = round(train_psnr/len(train_loader),5)
-    print(f'{epoch} Train Loss : {running_loss/len(train_loader):5f}, Train PSNR : {train_psnr}')
+        optimizer.step()
+    
+    print(f'{epoch} Train Loss : {running_loss/len(train_loader):5f}')
     
     # val data
-    model.eval()
-    psnr = 0
-    with torch.no_grad():
-        for lr, hr in tqdm(iter(val_loader)):
-            val_lr = lr.to(device)
-            val_hr = hr.to(device)
-            sr = model(val_lr)
-            psnr += cal_psnr(sr,val_hr)
-        
-    psnr = round(psnr/len(val_loader),5)
-    print(f'Val psnr : {psnr}')
+    if epoch %20 ==0:
+        model.eval()
+        psnr = 0
+        with torch.no_grad():
+            # 한 이미지씩 패치로
+            for lr_img, hr_img in tqdm(iter(val_loader)):
+                lr_img = lr_img.squeeze(0)
+                hr_img = hr_img.squeeze(0)
+                lr_img = einops.rearrange(lr_img, 'c (h p1) (w p2) -> (h w) c p1 p2', p1=64,p2=64)
+                pred_hr_img = []
+                # [64 , 3, 64, 64]
+                for i in range(lr_img.shape[0]):
+                    val_lr = lr_img[i].to(device)
+                    out = model(val_lr)
 
-    history['loss'].append(running_loss)
-    history['train_psnr'].append(train_psnr)
-    history['val_psnr'].append(psnr)
+                    pred_hr_img.append(out.detach().cpu().numpy())
+                # [64, 3, 256, 256]
+                pred = einops.rearrange(pred_hr_img, '(h w) c p1 p2 -> c (h p1) (w p2)', h=8, w=8)
 
-    if best_psnr < psnr:
-        torch.save({
-            'epoch': epoch,
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'loss': loss,
-            },f'./checkpoint/best_EDSR_{psnr}.pth')
-        print('Model Saved')
-        best_psnr = psnr
-        count = 1
-    else:
-        if count > 5:
-            print('Early Stopping')
-            break
-        count +=1
+                psnr += cal_psnr(pred,hr_img.numpy())
+
+        psnr = round(psnr/len(val_loader),5)
+        print(f'Val psnr : {psnr}')
+
+        history['loss'].append(running_loss)
+        history['val_psnr'].append(psnr)
+
+
+        if best_psnr < psnr:
+            best_psnr = psnr
+            count = 1
+            if psnr > 23:
+                torch.save({
+                    'epoch': epoch,
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'loss': loss,
+                    },f'./checkpoint/best_EDSR_{psnr}.pth')
+                print('Model Saved')
+        else:
+            if count > 5:
+                print('Early Stopping')
+                break
+            count +=1
     
